faebb1d8f835c5ed62783d2e1fb4eed8b64e3b25
dmitri.vronski <dmitri.vronski@2e47d5f7-414c-488b-9267-6fd57c54764d>
2017-10-30 16:09:17 +0000

    Initial
    git-svn-id: svn+ssh://192.168.161.250/Daten/VV/svn_repositories/git_test/trunk@3 2e47d5f7-414c-488b-9267-6fd57c54764d

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Dmitri Vronski on 30.10.17.
+//  Copyright © 2017 Dmitri Vronski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by Dmitri Vronski on 30.10.17.
+//  Copyright © 2017 Dmitri Vronski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
